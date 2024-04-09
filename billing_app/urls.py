3a0759d3e39e1f46c4d4f696116f70f647341193
unknown <ashishkr9214jmp@gmail.com>
2024-04-09 20:48:40 +0530

    accounts url

--- billing_app/urls.py
+++ billing_app/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('bills/', BillCreateView.as_view()),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('accounts/', include('django.contrib.auth.urls')),  # Includes built-in authentication views
 ]
 
 # path('login/', CustomAuthTokenLogin.as_view()),
