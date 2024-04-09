86977b54e3da410ae367938635d77d38cceb33c4
unknown <ashishkr9214jmp@gmail.com>
2024-04-09 20:36:33 +0530

    removed authentication

--- billing_app/urls.py
+++ billing_app/urls.py
@@ -19,14 +19,14 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
+   permission_classes=(permissions.AllowAny,),  # Allow unauthenticated access
 )
+
 
 urlpatterns = [
     path('', include(router.urls)),
     path('bills/', BillCreateView.as_view()),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # path('login/', CustomAuthTokenLogin.as_view()),
